3f332db3460968acea492244d458fa85858a7e2c
Allochka11 <allavelychko11@gmail.com>
2022-02-04 16:38:21 +0200

    sum of negative and positive numbers

---
+++ 01.js
@@ -0,0 +1,17 @@
+function countPositivesSumNegatives(input) {
+   if (input == null || input.length <1) {
+       return [];
+   }
+   var array = [0,0];
+
+   for (var i = 0; i < input.length; i++) {
+
+       if (input[i] <=0 ) {
+           array[1] += input[i];
+       } else {
+           array[0] += 1;
+       }
+   }
+    console.log(array);
+}
+countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])
