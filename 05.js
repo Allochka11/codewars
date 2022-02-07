020022477edd5ef56daf7dea57898a6dde11d1bd
Allochka11 <allavelychko11@gmail.com>
2022-02-07 19:44:43 +0200

    reverce word

--- 05.js
+++ 05.js
@@ -1,3 +1,13 @@
 function solution(str){
     return str.split('').reverse().join('');
 }
+
+function solution2(str){
+    var reversed = '';
+    for(var i = str.length - 1; i>=0 ; i--) {
+        reversed += str[i];
+    }
+    return reversed
+}
+
+console.log(solution2('billboard'));
